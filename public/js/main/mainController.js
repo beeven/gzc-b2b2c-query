84b251fb7beccc9e8b8e34d0e09cc37f632a18c8
zx2280 <wo19900822@gmail.com>
2014-09-03 09:33:25 +0800

    Rejected commit: Default

---
+++ public/js/main/mainController.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zzzxj on 8/19/2014.
+ */
+'use strict';
+
+angular.module('main.module', ['common.service.module'])
+    .controller('inputController', ['$scope',
+        function ($scope) {
+
+        }
+    ])
+    .controller('resultController', ['$scope',
+        function ($scope) {
+
+        }
+    ])
