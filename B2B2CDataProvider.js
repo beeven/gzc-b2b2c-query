06b2a1fef98dff3a6f5a01f6eed089b6f33711d5
beeven <beeven@hotmail.com>
2014-09-04 12:37:17 +0800

    Update OracleDataProvider

--- B2B2CDataProvider.js
+++ B2B2CDataProvider.js
@@ -16,6 +16,39 @@ var config = {
 
 
 
-exports.query = function(id,startDate,endDate){
+exports.query = function(criterion,startDate,endDate){
+    var start = Date.parse(startDate);
+    var end = Date.parse(endDate);
+    var deferred = Q.defer();
 
+    oracle.connect(config,function(err,connection){
+        if(err) {
+            console.error("Error connection to db:", err);
+            deferred.reject(err);
+        }
+        connection.execute(
+            'select OID as "Id", ' +
+            'RECEIVE_NAME as "receiver_name", ' +
+            'RECEIVE_NUM as "receiver_id", ' +
+            'TAX_NUMBER as "tax_number", ' +
+            'TAX_TOTAL as "tax_total", ' +
+            'POST_SUM as "post_sum", ' +
+            'RATEABLE_TOTAL as "rateable_total", ' +
+            'TAXABLE_DATE as "taxable_date" ' +
+            'from COM_EB_POSTITEMS_TAX ' +
+            'where RECEIVE_NAME like (:1) ' +
+            'or RECEIVE_NUM like (:2) ' +
+            'order by TAXABLE_DATE desc',
+            [criterion,criterion,start,end],
+            function(err, results){
+                if(err) {
+                    console.error("Error executing query: ",err);
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(results);
+                }
+                connection.close();
+            });
+    });
+    return deferred.promise;
 };
