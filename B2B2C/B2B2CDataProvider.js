266ef38549eb4f5cf877436f7830a8389b89d5c9
beeven <beeven@hotmail.com>
2014-09-04 16:04:08 +0800

    Update DataProvider

--- B2B2C/B2B2CDataProvider.js
+++ B2B2C/B2B2CDataProvider.js
@@ -19,6 +19,36 @@ var config = {
 exports.query = function(criterion,startDate,endDate){
     var start = Date.parse(startDate);
     var end = Date.parse(endDate);
+
+    var queryString = 'select OID as "Id", ' +
+        'RECEIVE_NAME as "receiver_name", ' +
+        'RECEIVE_NUM as "receiver_id", ' +
+        'TAX_NUMBER as "tax_number", ' +
+        'TAX_TOTAL as "tax_total", ' +
+        'POST_SUM as "post_sum", ' +
+        'RATEABLE_TOTAL as "rateable_total", ' +
+        'TAXABLE_DATE as "taxable_date" ' +
+        'from COM_EB_POSTITEMS_TAX ' +
+        'where (RECEIVE_NAME like (:1) or RECEIVE_NUM like (:2)) ';
+
+    var parameters = [criterion,criterion];
+
+    if(!!start && !end) {
+        queryString += "and TAXABLE_DATE >= (:3) ";
+        parameters.push(start);
+    }
+    else if(!start && !!end) {
+        queryString += "and TAXABLE_DATE <= (:3) ";
+        parameters.push(end);
+    }
+    else if(!!start && !!end){
+        queryString += "and TAXABLE_DATE between (:3) and (:4) ";
+        parameters.push(start);
+        parameters.push(end);
+    }
+
+    queryString += "order by TAXABLE_DATE desc";
+
     var deferred = Q.defer();
 
     oracle.connect(config,function(err,connection){
@@ -26,20 +56,10 @@ exports.query = function(criterion,startDate,endDate){
             console.error("Error connection to db:", err);
             deferred.reject(err);
         }
+
         connection.execute(
-            'select OID as "Id", ' +
-            'RECEIVE_NAME as "receiver_name", ' +
-            'RECEIVE_NUM as "receiver_id", ' +
-            'TAX_NUMBER as "tax_number", ' +
-            'TAX_TOTAL as "tax_total", ' +
-            'POST_SUM as "post_sum", ' +
-            'RATEABLE_TOTAL as "rateable_total", ' +
-            'TAXABLE_DATE as "taxable_date" ' +
-            'from COM_EB_POSTITEMS_TAX ' +
-            'where RECEIVE_NAME like (:1) ' +
-            'or RECEIVE_NUM like (:2) ' +
-            'order by TAXABLE_DATE desc',
-            [criterion,criterion,start,end],
+            queryString,
+            parameters,
             function(err, results){
                 if(err) {
                     console.error("Error executing query: ",err);
