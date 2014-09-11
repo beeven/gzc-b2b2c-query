474db609903352bb38c90d9081084209e5864be6
Beeven Yip <beeven@hotmail.com>
2014-09-11 15:49:58 +0800

    Update test

---
+++ test/B2B2C/B2B2CDataProvider.test.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Beeven on 9/3/2014.
+ */
+var dataProvider = require("../../B2B2C/B2B2CDummyDataProvider");
+
+describe("B2B2CDataProvider",function(){
+
+
+    it("should have a method naming query",function(){
+        dataProvider.should.have.a.property("query");
+        dataProvider.query.should.be.a.Function;
+    });
+
+    describe("B2B2CDataProvider.query",function(){
+        it("should have 3 arguments",function(){
+            dataProvider.query.should.have.lengthOf(3);
+        });
+
+        it("should return a promise if invoked",function(done){
+            var ret = dataProvider.query("abc","2014-03-21","2014-09-20");
+            ret.should.be.an.Object;
+            ret.should.have.a.property("then");
+            ret.then(function(data){
+                data.should.be.an.Array.with.a.property("length").which.is.above(0);
+                done();
+            });
+        });
+
+    });
+});
