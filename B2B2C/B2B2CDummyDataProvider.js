2f60820e6c975b600f9ec1c6465be7f1622adfdd
beeven <beeven@hotmail.com>
2014-09-16 11:15:30 +0800

    Updated B2B2CDummyDataProvider

--- B2B2C/B2B2CDummyDataProvider.js
+++ B2B2C/B2B2CDummyDataProvider.js
@@ -5,110 +5,103 @@
 var Q = require("q");
 
 
-function makeData(name,id){
+function makeData(id){
     return [
         {
-            receiver_name: name, // 收件人名称
+            tax_bill_id: null,
+            tax_total: 0,
+            date_of_issue: "2014-09-16 03:10:21",
             receiver_id: id,
-            tax_number: "P518520140400000009", // 个人税单号，应征税额为0时，此处 null
-            tax_total: 1194.20, // 计税物品总值
-            post_sum: 119.42, // 行邮税税额
-            rateable_total: 119.42, // 应征税额
-            taxable_date: new Date("2014-09-03") // 计税日期
+            orders: [
+                {
+                    order_id: "731197933138950",
+                    freight_id: "defghijk",
+                    status: "放行",
+                    last_updated: "2014-09-16 00:10:21"
+                }
+            ]
         },
         {
-            receiver_name: name, // 收件人名称
+            tax_bill_id: "P514120140700000106",
+            tax_total: "137.5",
+            date_of_issue: "2014-09-16 01:10:21",
             receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 312, // 计税物品总值
-            post_sum: 31.2, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-09-02") // 计税日期
+            orders:[
+                {
+                    order_id: "713195292078950",
+                    freight_id: "abcdefg",
+                    status: "放行",
+                    last_updated: "2014-09-15 12:50:21"
+                },
+                {
+                    order_id: "713195292078950-1",
+                    freight_id: "abcdefg",
+                    status: "放行",
+                    last_updated: "2014-09-15 12:49:22"
+                },
+                {
+                    order_id: "713195292078950-2",
+                    freight_id: "abcdefg",
+                    status: "放行",
+                    last_updated: "2014-09-15 15:22:28"
+                }
+            ]
         },
         {
-            receiver_name: name, // 收件人名称
+            tax_bill_id: null,
+            tax_total: 0,
+            date_of_issue: "2014-09-16 00:10:21",
             receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 213.40, // 计税物品总值
-            post_sum: 21.34, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-09-01") // 计税日期
+            orders: [
+                {
+                    order_id: "731197933138950",
+                    freight_id: "defghijk",
+                    status: "放行",
+                    last_updated: "2014-09-16 00:10:21"
+                }
+            ]
         },
         {
-            receiver_name: name, // 收件人名称
+            tax_bill_id: null,
+            tax_total: 0,
+            date_of_issue: "2014-09-15 00:09:21",
             receiver_id: id,
-            tax_number: "P518520140300000023", // 个人税单号，应征税额为0时，此处 null
-            tax_total: 3194.20, // 计税物品总值
-            post_sum: 319.42, // 行邮税税额
-            rateable_total: 319.42, // 应征税额
-            taxable_date: new Date("2014-08-30") // 计税日期
+            orders: [
+                {
+                    order_id: "751197933138950",
+                    freight_id: "defghijk",
+                    status: "放行",
+                    last_updated: "2014-09-15 00:09:21"
+                }
+            ]
         },
         {
-            receiver_name: name, // 收件人名称
+            tax_bill_id: "P514120140700000003",
+            tax_total: "120",
+            date_of_issue: "2014-09-14 01:10:21",
             receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 194.20, // 计税物品总值
-            post_sum: 19.42, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-08-29") // 计税日期
-        },
-        {
-            receiver_name: name, // 收件人名称
-            receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 194.20, // 计税物品总值
-            post_sum: 19.42, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-08-29") // 计税日期
-        },
-        {
-            receiver_name: name, // 收件人名称
-            receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 194.20, // 计税物品总值
-            post_sum: 19.42, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-08-29") // 计税日期
-        },
-        {
-            receiver_name: name, // 收件人名称
-            receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 194.20, // 计税物品总值
-            post_sum: 19.42, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-08-29") // 计税日期
-        },
-        {
-            receiver_name: name, // 收件人名称
-            receiver_id: id,
-            tax_number: null, // 个人税单号，应征税额为0时，此处 null
-            tax_total: 194.20, // 计税物品总值
-            post_sum: 19.42, // 行邮税税额
-            rateable_total: 0, // 应征税额
-            taxable_date: new Date("2014-08-29") // 计税日期
+            orders:[
+                {
+                    order_id: "713195292078950",
+                    freight_id: "abcdefg",
+                    status: "放行",
+                    last_updated: "2014-09-14 12:50:21"
+                },
+                {
+                    order_id: "713195292078950-1",
+                    freight_id: "abcdefg",
+                    status: "放行",
+                    last_updated: "2014-09-14 12:49:22"
+                }
+            ]
         }
     ];
 }
 
 
 exports.query = function(criterion,startDate,endDate) {
-    var name="奥巴马";
-    var id = "350204197705306527";
-    if(/[a-zA-Z0-9]{13}/.test(id)) {
-        id = criterion;
-    } else {
-        name = criterion;
-    }
-    var start = Date.parse(startDate);
-    var end = Date.parse(endDate);
-    var data = makeData(name,id);
-    if(!isNaN(start)) {
-        data = data.filter(function(x){return x.taxable_date >= start});
-    }
-    if(!isNaN(end)) {
-        data = data.filter(function(x){return x.taxable_date <= end});
-    }
+
+    var data = makeData(criterion);
 
     return Q(data);
 };
