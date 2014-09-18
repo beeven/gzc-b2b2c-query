90487682be3e4a8fc2c96d342c781d7eeba4c2cb
beeven <beeven@hotmail.com>
2014-09-18 17:33:10 +0800

    Update EdgeDataProvider logic

--- B2B2C/B2B2CEdgeDataProvider.cs
+++ B2B2C/B2B2CEdgeDataProvider.cs
@@ -33,24 +33,35 @@ public class Startup
         {
             startDate = DateTime.Now.Subtract(TimeSpan.FromDays(30));
         }
-        if(!DateTime.TryParse(end, out endDate))
+        if (!DateTime.TryParse(end, out endDate))
         {
             endDate = DateTime.Now;
         }
-        
-        return await Helper.Query(criterion,startDate,endDate);
+
+        return await Helper.Query(criterion, startDate, endDate);
     }
 }
 
 static class Helper
 {
-    static string connecitonString = "Data Source=10.53.1.194/ZNHG;User ID=kjecsel;Password=kjecsel;";
+    static string bbcConnString = "Data Source=10.53.1.194/ZNHG;User ID=kjecsel;Password=kjecsel;";
+
+    static string bcConnString = "Data Source=10.53.1.194/ZNHG;User ID=gzhg;Password=gzhg;";
     public static async Task<TaxBill[]> Query(string criterion, DateTime startDate, DateTime endDate)
     {
-        using(OracleConnection dbcon = new OracleConnection(connecitonString))
+        var bbcBillsTask = QueryBBC(criterion, startDate, endDate);
+        var bcBillsTask = QueryBC(criterion, startDate, endDate);
+        var taxBills = await Task.WhenAll(bbcBillsTask, bcBillsTask);
+        return taxBills.SelectMany(x => x.AsEnumerable()).OrderByDescending(x => x.date_of_issue).ToArray();
+    }
+
+    public static async Task<List<TaxBill>> QueryBBC(string criterion, DateTime startDate, DateTime endDate)
+    {
+        Dictionary<String, TaxBill> groups = new Dictionary<string, TaxBill>();
+        List<TaxBill> results = new List<TaxBill>();
+        using (OracleConnection dbcon = new OracleConnection(bbcConnString))
         {
-            Dictionary<String, TaxBill> groups = new Dictionary<string, TaxBill>();
-            List<TaxBill> results = new List<TaxBill>();
+
             await dbcon.OpenAsync();
             OracleCommand dbcmd = dbcon.CreateCommand();
             string sql = "select ORDERDOCID as \"receiver_id\", " +
@@ -62,26 +73,27 @@ static class Helper
                      "PRINT_DATE as \"date_of_issue\", " +
                      "TAX_NUMBER as \"tax_bill_id\" " +
                      "from KJECCUS.V_EBILL_FOR_WECHAT " +
-                     "where ORDERDOCID = :criterion "+
-                     "and ( HANDLE_DATE is null or HANDLE_DATE between :startDate and :endDate )"+
+                     "where ORDERDOCID = :criterion " +
+                     "and ( HANDLE_DATE is null or HANDLE_DATE between :startDate and :endDate )" +
                      "order by HANDLE_DATE desc";
             dbcmd.CommandText = sql;
             dbcmd.Parameters.Add(new OracleParameter("criterion", criterion));
             dbcmd.Parameters.Add(new OracleParameter("startDate", startDate));
             dbcmd.Parameters.Add(new OracleParameter("endDate", endDate));
             OracleDataReader reader = dbcmd.ExecuteReader();
-            while(await reader.ReadAsync())
+            while (await reader.ReadAsync())
             {
                 string tax_bill_id = reader["tax_bill_id"] as string;
                 if (tax_bill_id != null)
                 {
-                    if(!groups.ContainsKey(tax_bill_id))
+                    if (!groups.ContainsKey(tax_bill_id))
                     {
-                        groups.Add(tax_bill_id, new TaxBill() {
+                        groups.Add(tax_bill_id, new TaxBill()
+                        {
                             tax_bill_id = tax_bill_id,
                             date_of_issue = reader["date_of_issue"] as DateTime?,
-                            tax_total = (Decimal)reader["tax_total"] ,
-                            orders = new List<Order>() 
+                            tax_total = (Decimal)reader["tax_total"],
+                            orders = new List<Order>()
                         });
                     }
                     TaxBill tb = groups[(string)reader[7]];
@@ -109,18 +121,98 @@ static class Helper
             }
             reader.Close();
             dbcmd.Dispose();
+        }
 
-            foreach(var tb in groups.Values)
+        foreach (var tb in groups.Values)
+        {
+            if (tb.date_of_issue == null)
             {
-                if(tb.date_of_issue == null)
+                tb.date_of_issue = tb.orders.Max(x => x.last_updated);
+            }
+        }
+
+        results.AddRange(groups.Values);
+        return results;
+    }
+
+    public static async Task<List<TaxBill>> QueryBC(string criterion, DateTime startDate, DateTime endDate)
+    {
+        Dictionary<String, TaxBill> groups = new Dictionary<string, TaxBill>();
+        List<TaxBill> results = new List<TaxBill>();
+        using (OracleConnection dbcon = new OracleConnection(bcConnString))
+        {
+
+            await dbcon.OpenAsync();
+            OracleCommand dbcmd = dbcon.CreateCommand();
+            string sql = "select V_RECEIVER_CARDNO as \"receiver_id\", " +
+                     "V_ORDER_NO as \"order_id\", " +
+                     "V_FREIGHT_NO as \"freight_id\", " +
+                     "D_LASTEDITTIME as \"last_updated\", " +
+                     "V_STATUS as \"status\", " +
+                     "N_TAX_MONEY as \"tax_total\", " +
+                     "D_PERSONTAXDATE as \"date_of_issue\", " +
+                     "V_TAX_NO as \"tax_bill_id\" " +
+                     "from V_CONSIGNOR_INFO " +
+                     "where V_RECEIVER_CARDNO = :criterion " +
+                     "and ( D_PERSONTAXDATE is null or D_PERSONTAXDATE between :startDate and :endDate )" +
+                     "order by D_PERSONTAXDATE desc";
+            dbcmd.CommandText = sql;
+            dbcmd.Parameters.Add(new OracleParameter("criterion", criterion));
+            dbcmd.Parameters.Add(new OracleParameter("startDate", startDate));
+            dbcmd.Parameters.Add(new OracleParameter("endDate", endDate));
+            OracleDataReader reader = dbcmd.ExecuteReader();
+            while (await reader.ReadAsync())
+            {
+                string tax_bill_id = reader["tax_bill_id"] as string;
+                if (tax_bill_id != null)
                 {
-                    tb.date_of_issue = tb.orders.Max(x=>x.last_updated);
+                    if (!groups.ContainsKey(tax_bill_id))
+                    {
+                        groups.Add(tax_bill_id, new TaxBill()
+                        {
+                            tax_bill_id = tax_bill_id,
+                            date_of_issue = reader["date_of_issue"] as DateTime?,
+                            tax_total = (Decimal)reader["tax_total"],
+                            orders = new List<Order>()
+                        });
+                    }
+                    TaxBill tb = groups[(string)reader[7]];
+                    tb.orders.Add(new Order()
+                    {
+                        order_id = reader["order_id"] as string,
+                        freight_id = reader["freight_id"] as string,
+                        status = reader["status"] as string,
+                        last_updated = reader["last_updated"] as DateTime?
+                    });
+                }
+                else
+                {
+                    results.Add(new TaxBill()
+                    {
+                        date_of_issue = reader["last_updated"] as DateTime?,
+                        orders = new List<Order>() { new Order() { 
+                            order_id = reader["order_id"] as string,
+                            freight_id = reader["freight_id"] as string,
+                            status = reader["status"] as string,
+                            last_updated = reader["last_updated"] as DateTime?
+                        } }
+                    });
                 }
             }
-
-            results.AddRange(groups.Values);
-            return results.OrderByDescending(x=>x.date_of_issue).ToArray();
+            reader.Close();
+            dbcmd.Dispose();
         }
+
+        foreach (var tb in groups.Values)
+        {
+            if (tb.date_of_issue == null)
+            {
+                tb.date_of_issue = tb.orders.Max(x => x.last_updated);
+            }
+        }
+
+        results.AddRange(groups.Values);
+        return results;
     }
 }
 class Program
@@ -128,13 +220,16 @@ class Program
     static void Main(string[] args)
     {
         //var t = Helper.Query("320981198706300469",DateTime.Now.Subtract(TimeSpan.FromDays(30)),DateTime.Now);
-        var t = Helper.Query("320981198706300469",DateTime.Now.Subtract(TimeSpan.FromDays(180)),DateTime.Now);
+        //var t = Helper.Query("320981198706300469", DateTime.Now.Subtract(TimeSpan.FromDays(180)), DateTime.Now);
+        var t = Helper.Query("440881198507190051", DateTime.Now.Subtract(TimeSpan.FromDays(180)), DateTime.Now);
         t.Wait();
         var results = t.Result;
-        foreach(var r in results)
+        foreach (var r in results)
         {
             Console.WriteLine("Tax_Bill_Id: {0} \t Orders: {1} \t DateOfIssue: {2}", r.tax_bill_id, r.orders.Count, r.date_of_issue);
         }
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
     }
 }
 
