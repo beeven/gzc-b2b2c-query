6607b36758c17a3be6d20affd64d409e56637c08
Beeven Yip <beeven@hotmail.com>
2015-03-12 22:47:55 +0800

    Added String.prototype.repeat for IE

--- public/js/common/spinner.js
+++ public/js/common/spinner.js
@@ -5,8 +5,23 @@
 	"use strict";
 	angular.module('spinner',[])
 	.directive('spinner',function(){
+        var content;
+        function repeat(pattern ,count) {
+            if(count<1) return '';
+            var result = '';
+            while (count > 1) {
+                if(count & 1) result += pattern;
+                count >>= 1, pattern += pattern;
+            }
+            return result + pattern;
+        }
+        if(!String.prototype.repeat) {
+            content = repeat("<div class='cube'></div>",9);
+        } else {
+            content = "<div class='cube'></div>".repeat(9);
+        }
 		return {
-			template:"<div class='spinner'>" + "<div class='cube'></div>".repeat(3*3) + "</div>",
+			template:"<div class='spinner'>" + content + "</div>",
 			restrict: 'A',
 			scope: {
 				style: "@spinner"
